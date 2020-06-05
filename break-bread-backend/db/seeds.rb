acfc8ea25e29fd769f780f8b9b24e5479a523668
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-05 03:02:45 -0700

    Created business seeds

--- break-bread-backend/db/seeds.rb
+++ break-bread-backend/db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-City.create([{ name: 'Los Angeles', abbrev: 'LA' }, { name: 'San Francisco', abbrev: 'SF' }])
+# City.create([{ name: 'Los Angeles', abbrev: 'LA' }, { name: 'San Francisco', abbrev: 'SF' }])
 
-Neighborhood.create([{ name: 'Alhambra', city_id: 1 }, { name: 'Altadena', city_id: 1 }])
+# Neighborhood.create([{ name: 'Alhambra', city_id: 1 }, { name: 'Altadena', city_id: 1 }])
+
+Business.create([{
+                  name: "Wilson's Bbq Rib Shack", phone: "(323) 457-0258", website: "https://www.wilsonsbbqribshack.com", instagram: "@wilsonsbbqribshack", neighborhood_id: 1
+                 }, 
+
+                 {
+                  name: "Little Red Hen Coffee Shop", phone: "(626) 791-1614" , website: "https://little-red-hen-coffee-shop.hub.biz/?fbclid=IwAR1MZtXbf2HM5eY6W5hX91uaCIFxG1QcAhHpu235BwavmVMhjEzPmEYDPFM" , instagram: "@thelittleredhencoffeeshop", neighborhood_id: 2
+                 }])
+
+
+
+
