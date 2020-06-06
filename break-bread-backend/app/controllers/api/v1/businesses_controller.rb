c5d586ebca95123ca222c3c087fb6434a1805cc8
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-05 19:41:09 -0700

    Added create action in businesses controller and routes

--- break-bread-backend/app/controllers/api/v1/businesses_controller.rb
+++ break-bread-backend/app/controllers/api/v1/businesses_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::BusinessesController < ApplicationController
     businesses = Business.all
     render json: businesses
   end
+
+  def create
+    business = Business.new(business_params)
+
+    if business.save
+      render json: business, status: :accepted 
+    else
+      render json: {errors: business.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def business_params
+    params.permit(:business).require(:name, :phone, :website, :instagram, :neighborhood_id)
+  end
 end
--- break-bread-backend/config/routes.rb
+++ break-bread-backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :businesses, only: [:index]
+      resources :businesses, only: [:index, :create]
     end
   end
 end
