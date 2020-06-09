a483cd519465c0a68e38a931df3677726999cc16
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-09 02:59:07 -0700

    Created renderBusiness

--- breaking-bread-frontend/src/js/index.js
+++ breaking-bread-frontend/src/js/index.js
@@ -8,9 +8,31 @@ function fetchBusinesses() {
   fetch(url)
     .then(resp => resp.json())
     .then(json => {
-      console.log(typeof json)
-      debugger
+      json.forEach(business => {
+        renderBusiness(business)
+      });
+      // console.log(json)
+      // debugger
     })   
 }
+
+function renderBusiness(business) {  
+  const ul = document.getElementById("businessList")
+
+  const li = document.createElement("li");
+  li.dataset.id = business.id;
+  li.innerText = business.name;
+
+  ul.appendChild(li);
+
+
+}
+
+
+
+
+
+
+
 
 onLoad();
