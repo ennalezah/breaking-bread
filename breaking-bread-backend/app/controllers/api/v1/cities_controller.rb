fee56048eac41c26f9a08d75714526d28e649de7
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-11 03:58:20 -0700

    Added index action in cities controller and routes

--- breaking-bread-backend/app/controllers/api/v1/cities_controller.rb
+++ breaking-bread-backend/app/controllers/api/v1/cities_controller.rb
@@ -1,3 +1,6 @@
 class Api::V1::CitiesController < ApplicationController
-  
+  def index
+    cities = City.all
+    render json: cities
+  end
 end
--- breaking-bread-backend/config/routes.rb
+++ breaking-bread-backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :businesses, only: [:index, :show, :create]
+      resources :cities, only: [:index, :show]
     end
   end
 end
