5522257d78ad5439ecabd59bb9cade71629e3887
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-11 04:12:00 -0700

    Added show action in cities controller

--- breaking-bread-backend/app/controllers/api/v1/cities_controller.rb
+++ breaking-bread-backend/app/controllers/api/v1/cities_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::CitiesController < ApplicationController
     cities = City.all
     render json: CitySerializer.new(cities).to_serialized_json, status: :accepted
   end
+
+  def show
+    city = City.find_by(id: params[:id])
+
+    if city
+      render json: CitySerializer.new(city).to_serialized_json, status: :accepted
+    else
+      render json: "City not found. Please check the ID and try again.", status: :not_found
+    end
+  end
 end
