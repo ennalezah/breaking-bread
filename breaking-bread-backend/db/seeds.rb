a501589f89c99a228f0ffc50e6c900726c58225b
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-14 04:52:07 -0700

    Installed database_cleaner gem and added neighborhood seeds

--- breaking-bread-backend/db/seeds.rb
+++ breaking-bread-backend/db/seeds.rb
@@ -6,21 +6,97 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-City.destroy_all;
-Neighborhood.destroy_all;
-Business.destroy_all;
+require 'database_cleaner'
 
-City.create([{ name: 'Los Angeles', abbrev: 'LA' }, { name: 'San Francisco', abbrev: 'SF' }])
+DatabaseCleaner.clean_with(:truncation)
 
-Neighborhood.create([{ name: 'Alhambra', city_id: 1 }, { name: 'Altadena', city_id: 1 }])
+la = City.create(name: 'Los Angeles', abbrev: 'LA')
 
-Business.create([{
-                  name: "Wilson's Bbq Rib Shack", phone: "(323) 457-0258", website: "https://www.wilsonsbbqribshack.com", instagram: "@wilsonsbbqribshack", neighborhood_id: 1
-                 }, 
+la.neighborhoods.create([
+  { name: 'Alhambra' },
+  { name: 'Altadena' },
+  { name: 'Anaheim' },
+  { name: 'Atwater Village' },
+  { name: 'Azusa' },
+  { name: 'Baldwin Hills' },
+  { name: 'Bellflower' },
+  { name: 'Boyle Heights' },
+  { name: 'Burbank' },
+  { name: 'Canoga Park' },
+  { name: 'Carson' },
+  { name: 'Chatsworth' },
+  { name: 'Compton' },
+  { name: 'Corona' },
+  { name: 'Covina' },
+  { name: 'Crenshaw' },
+  { name: 'Culver City' },
+  { name: 'Downtown' },
+  { name: 'Historic Filipinotown' },
+  { name: 'Echo Park' },
+  { name: 'Exposition Park' },
+  { name: 'Fairfax' },
+  { name: 'Gardena' },
+  { name: 'Glendale' },
+  { name: 'Gramercy Park' },
+  { name: 'Hawthorne' },
+  { name: 'Hollywood' },
+  { name: 'Huntington Beach' },
+  { name: 'Huntington Park' },
+  { name: 'Hyde Park' },
+  { name: 'Inglewood' },
+  { name: 'Jefferson Park' },
+  { name: 'La Cienega Heights' },
+  { name: 'Lakewood' },
+  { name: 'Lancaster' },
+  { name: 'Leimert Park' },
+  { name: 'Little Ethiopia' },
+  { name: 'Long Beach' },
+  { name: 'Manchester' },
+  { name: 'Marina Del Ray' },
+  { name: 'MacArthur Park' },
+  { name: 'Mid-City' },
+  { name: 'Mid-Wilshire' },
+  { name: 'Morningside Park' },
+  { name: 'North Hollywood' },
+  { name: 'Palmdale' },
+  { name: 'Paramount' },
+  { name: 'Park-Windsor Hills' },
+  { name: 'Pasadena' },
+  { name: 'Playa Vista' },
+  { name: 'Pomona' },
+  { name: 'Santa Ana' },
+  { name: 'Santa Clarita' },
+  { name: 'Sawtelle' },
+  { name: 'Sherman Oaks' },
+  { name: 'South Gate' },
+  { name: 'South LA' },
+  { name: 'Silver Lake' },
+  { name: 'Studio City' },
+  { name: 'Sylmar' },
+  { name: 'Tarzana' },
+  { name: 'Thai Town' },
+  { name: 'Toluca Lake' },
+  { name: 'Torrance' },
+  { name: 'Van Ness' },
+  { name: 'Van Nuys' },
+  { name: 'Watts' },
+  { name: 'West Adams' },
+  { name: 'Westchester' },
+  { name: 'West Hollywood' },
+  { name: 'Windsor Hills' }
+  ])
+
+# City.create([{ name: 'Los Angeles', abbrev: 'LA' }, { name: 'San Francisco', abbrev: 'SF' }])
+
+# Neighborhood.create([{ name: 'Alhambra', city_id: 1 }, { name: 'Altadena', city_id: 1 }])
+
+# Business.create([{
+#                   name: "Wilson's Bbq Rib Shack", phone: "(323) 457-0258", website: "https://www.wilsonsbbqribshack.com", instagram: "@wilsonsbbqribshack", neighborhood_id: 1
+#                  }, 
            
-                 {
-                  name: "Little Red Hen Coffee Shop", phone: "(626) 791-1614" , website: "https://little-red-hen-coffee-shop.hub.biz/?fbclid=IwAR1MZtXbf2HM5eY6W5hX91uaCIFxG1QcAhHpu235BwavmVMhjEzPmEYDPFM" , instagram: "@thelittleredhencoffeeshop", neighborhood_id: 2
-                 }])
+#                  {
+#                   name: "Little Red Hen Coffee Shop", phone: "(626) 791-1614" , website: "https://little-red-hen-coffee-shop.hub.biz/?fbclid=IwAR1MZtXbf2HM5eY6W5hX91uaCIFxG1QcAhHpu235BwavmVMhjEzPmEYDPFM" , instagram: "@thelittleredhencoffeeshop", neighborhood_id: 2
+#                  }])
 
 
 
