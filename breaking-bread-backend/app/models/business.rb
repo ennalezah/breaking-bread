9518d1e203e7905fe23c76dab74316ac52bce062
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-14 18:10:17 -0700

    Added validations for business name

--- breaking-bread-backend/app/models/business.rb
+++ breaking-bread-backend/app/models/business.rb
@@ -1,3 +1,12 @@
 class Business < ApplicationRecord
   belongs_to :neighborhood
+
+  validates :name, presence: true
+  before_validation :titleize_name
+
+  private
+
+  def titleize_name
+    self.name = name.titleize
+  end
 end
