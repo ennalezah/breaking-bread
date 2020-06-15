d225f6a37d6f3d926415f077f14cc3a46c2be177
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-14 19:55:15 -0700

    Added validations for business phone

--- breaking-bread-backend/app/models/business.rb
+++ breaking-bread-backend/app/models/business.rb
@@ -2,11 +2,25 @@ class Business < ApplicationRecord
   belongs_to :neighborhood
 
   validates :name, presence: true
-  before_validation :titleize_name
+
+  # validates :phone, 
+  #   length: { is: 10, wrong_length: "Must contain %{count} digits" },
+  #   format: { with: /\A[0-9]+\z/, message: "Only allows numbers" }
+
+  before_validation :titleize_name, :format_phone
+  
 
   private
 
   def titleize_name
     self.name = name.titleize
+  end
+
+  def format_phone
+    p = phone.scan(/\d/)
+    areaCode = p[0..2].join
+    prefix = p[3..5].join
+    lineNumber = p[6..9].join
+    self.phone = "(#{areaCode}) #{prefix}-#{lineNumber}"    
   end
 end
