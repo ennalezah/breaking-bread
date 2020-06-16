170e154390f4660dce51c05338f5de8a6c37834a
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-16 00:32:52 -0700

    Created neighborhood serializer and added to index controller action

--- breaking-bread-backend/app/controllers/api/v1/neighborhoods_controller.rb
+++ breaking-bread-backend/app/controllers/api/v1/neighborhoods_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NeighborhoodsController < ApplicationController
 
   def index
     neighborhoods = Neighborhood.all
-    render json: neighborhoods
+    render json: NeighborhoodSerializer.new(neighborhoods).to_serialized_json, status: :accepted
   end
 
   def show
---
+++ breaking-bread-backend/app/serializers/neighborhood_serializer.rb
@@ -0,0 +1,19 @@
+class NeighborhoodSerializer
+
+  def initialize(neighborhood_object)
+    @neighborhood = neighborhood_object
+  end
+
+  def to_serialized_json    
+    options = {
+      include: { 
+        businesses: {
+          only: [:id, :name]
+        }
+      },
+      except: [:city_id, :created_at, :updated_at]
+    }
+
+    @neighborhood.to_json(options)
+  end
+end
