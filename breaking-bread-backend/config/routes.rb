3284b580c289bbba602c9ae6c83939c7a02f1151
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-06-16 00:21:42 -0700

    Removed create route for neighborhood

--- breaking-bread-backend/config/routes.rb
+++ breaking-bread-backend/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do      
       resources :cities, only: [:index, :show]
-      resources :neighborhoods, only: [:index, :show, :create]
+      resources :neighborhoods, only: [:index, :show]
       resources :businesses, only: [:index, :show, :create]
     end
   end
